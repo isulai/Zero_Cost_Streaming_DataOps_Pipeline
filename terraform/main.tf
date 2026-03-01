bb1837a77b7bef6a2d0597525e3b4b15fd67121e
isulai <sulaimanpno@gmail.com>
2026-03-01 14:58:39 +0000

    serverless env

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,20 @@
 resource "databricks_job" "medallion_pipeline_job" {
   name = "Automated_Medallion_Pipeline"
 
+  # We define the Serverless environment here
+  environment {
+    environment_key = "serverless_env"
+    spec {
+      client = "1"
+      dependencies = [
+        "pip"
+      ]
+    }
+  }
+
   task {
     task_key = "bronze_ingestion"
+    environment_key = "serverless_env" # Tell the task to use the environment
     spark_python_task {
       python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/bronze.py"
     }
@@ -13,6 +25,7 @@ resource "databricks_job" "medallion_pipeline_job" {
     depends_on {
       task_key = "bronze_ingestion"
     }
+    environment_key = "serverless_env"
     spark_python_task {
       python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/silver.py"
     }
@@ -23,6 +36,7 @@ resource "databricks_job" "medallion_pipeline_job" {
     depends_on {
       task_key = "silver_transformation"
     }
+    environment_key = "serverless_env"
     spark_python_task {
       python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/gold.py"
     }
