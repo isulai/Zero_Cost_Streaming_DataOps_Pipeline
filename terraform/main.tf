9877b3ec3890a16338c5424fa95e2bfc98b08511
isulai <sulaimanpno@gmail.com>
2026-03-03 12:42:58 +0000

    Final fix hopefully

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,31 @@
+# This 'sniffs' the correct Spark version for your specific account
+data "databricks_spark_version" "latest" {
+  long_term_support = true
+}
+
+# This 'sniffs' the cheapest possible machine type allowed on your account
+data "databricks_node_type" "cheapest" {
+  local_disk = true
+}
+
 resource "databricks_job" "medallion_pipeline_job" {
   name = "Automated_Medallion_Pipeline"
 
+  # This creates a TEMPORARY cluster specifically for this run
+  job_cluster {
+    job_cluster_key = "auto_cluster"
+    new_cluster {
+      num_workers   = 1
+      spark_version = data.databricks_spark_version.latest.id
+      node_type_id  = data.databricks_node_type.cheapest.id
+    }
+  }
+
   task {
     task_key = "bronze_ingestion"
-    notebook_task {
-      notebook_path = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/bronze.py"
+    job_cluster_key = "auto_cluster"
+    spark_python_task {
+      python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/bronze.py"
     }
   }
 
@@ -13,8 +34,9 @@ resource "databricks_job" "medallion_pipeline_job" {
     depends_on {
       task_key = "bronze_ingestion"
     }
-    notebook_task {
-      notebook_path = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/silver.py"
+    job_cluster_key = "auto_cluster"
+    spark_python_task {
+      python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/silver.py"
     }
   }
 
@@ -23,8 +45,9 @@ resource "databricks_job" "medallion_pipeline_job" {
     depends_on {
       task_key = "silver_transformation"
     }
-    notebook_task {
-      notebook_path = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/gold.py"
+    job_cluster_key = "auto_cluster"
+    spark_python_task {
+      python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/gold.py"
     }
   }
 }
