7ad4dc5e99e08ad76425c62f000724f2f3bcc52c
isulai <sulaimanpno@gmail.com>
2026-03-03 13:41:13 +0000

    Removed the suffix on the files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,11 @@
-# This 'sniffs' the correct Spark version for your specific account
-data "databricks_spark_version" "latest" {
-  long_term_support = true
-}
-
-# This 'sniffs' the cheapest possible machine type allowed on your account
-data "databricks_node_type" "cheapest" {
-  local_disk = true
-}
-
 resource "databricks_job" "medallion_pipeline_job" {
   name = "Automated_Medallion_Pipeline"
 
-  # This creates a TEMPORARY cluster specifically for this run
-  job_cluster {
-    job_cluster_key = "auto_cluster"
-    new_cluster {
-      num_workers   = 1
-      spark_version = data.databricks_spark_version.latest.id
-      node_type_id  = data.databricks_node_type.cheapest.id
-    }
-  }
-
   task {
     task_key = "bronze_ingestion"
-    job_cluster_key = "auto_cluster"
-    spark_python_task {
-      python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/bronze.py"
+    notebook_task {
+      # Notice we REMOVED the .py extension from the path!
+      notebook_path = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/bronze"
     }
   }
 
@@ -34,9 +14,8 @@ resource "databricks_job" "medallion_pipeline_job" {
     depends_on {
       task_key = "bronze_ingestion"
     }
-    job_cluster_key = "auto_cluster"
-    spark_python_task {
-      python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/silver.py"
+    notebook_task {
+      notebook_path = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/silver"
     }
   }
 
@@ -45,9 +24,8 @@ resource "databricks_job" "medallion_pipeline_job" {
     depends_on {
       task_key = "silver_transformation"
     }
-    job_cluster_key = "auto_cluster"
-    spark_python_task {
-      python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/gold.py"
+    notebook_task {
+      notebook_path = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/gold"
     }
   }
 }
