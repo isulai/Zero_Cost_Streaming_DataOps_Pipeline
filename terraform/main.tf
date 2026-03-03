635d8be8c1f01bbc71664d771316923498558661
isulai <sulaimanpno@gmail.com>
2026-03-03 12:02:25 +0000

    Switched to Python Script Task

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,21 @@
 resource "databricks_job" "medallion_pipeline_job" {
   name = "Automated_Medallion_Pipeline"
 
+  # We define a clean Serverless Python environment here
+  environment {
+    environment_key = "serverless_python_env"
+    spec {
+      dependencies = [
+        "pip"
+      ]
+    }
+  }
+
   task {
     task_key = "bronze_ingestion"
-    notebook_task {
-      notebook_path = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/bronze.py"
+    environment_key = "serverless_python_env"
+    spark_python_task {
+      python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/bronze.py"
     }
   }
 
@@ -13,8 +24,9 @@ resource "databricks_job" "medallion_pipeline_job" {
     depends_on {
       task_key = "bronze_ingestion"
     }
-    notebook_task {
-      notebook_path = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/silver.py"
+    environment_key = "serverless_python_env"
+    spark_python_task {
+      python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/silver.py"
     }
   }
 
@@ -23,8 +35,9 @@ resource "databricks_job" "medallion_pipeline_job" {
     depends_on {
       task_key = "silver_transformation"
     }
-    notebook_task {
-      notebook_path = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/gold.py"
+    environment_key = "serverless_python_env"
+    spark_python_task {
+      python_file = "/Workspace/Users/sulaipno97@gmail.com/Sulaiman/Zero-Cost Streaming DataOps Pipeline - Project 1/Zero_Cost_Streaming_DataOps_Pipeline/src/gold.py"
     }
   }
 }
